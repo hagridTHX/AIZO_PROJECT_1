7eee1de09c4688ee8f4804b66fa27b04b8e1abb1
Franciszek Wojnowski <franekwojnowski@gmail.com>
2025-04-30 17:20:09 +0200

    completed

--- script.bat
+++ script.bat
@@ -13,25 +13,25 @@ set OUTPUT_DIR=output
 
 if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
 
-rem --- BADANIE 1: Wpływ liczebności zbioru ---
+rem  BADANIE 1:
 
 if false == true (
 
-    rem --- Wersje podstawowe (Insert, Heap) ---
+    rem Wersje podstawowe (Insert, Heap)
     for %%A in (0 1) do (
         for %%S in (10000 20000 40000 80000 160000) do (
             "%BINARY%" --test %%A 0 %%S %RUNS% 0 "%OUTPUT_DIR%\bad1_alg%%A.txt"
         )
     )
 
-    rem --- ShellSort (gap 0 i 1) ---
+    rem ShellSort (gap 0 i 1)
     for %%G in (0 1) do (
         for %%S in (10000 20000 40000 80000 160000) do (
             "%BINARY%" --test 2 0 %%S %RUNS% 0 --gap %%G "%OUTPUT_DIR%\bad1_shell_gap%%G.txt"
         )
     )
 
-    rem --- QuickSort (pivot 0,1,2,3) ---
+    rem QuickSort (pivot 0,1,2,3)
     for %%P in (0 1 2 3) do (
         for %%S in (10000 20000 40000 80000 160000) do (
             "%BINARY%" --test 3 0 %%S %RUNS% 0 --pivot %%P "%OUTPUT_DIR%\bad1_quick_pivot%%P.txt"
@@ -40,16 +40,61 @@ if false == true (
 
 )
 
-rem --- HeapDrunk (różne wartości drunk) ---
-if true == true (
+rem HeapDrunk (różne wartości drunk)
+if false == true (
     for %%D in (1 5 10 20 50) do (
         "%BINARY%" --test 4 0 40000 %RUNS% 0 --drunk %%D "%OUTPUT_DIR%\bad1_heapdrunk_drunk%%D.txt"
     )
 )
 
-if true == true (
-    for %%D in (1000 2500 5000 7500 10000) do (
+if false == true (
+    for %%D in (10000) do (
         "%BINARY%" --test 4 0 40000 %RUNS% 0 --drunk %%D "%OUTPUT_DIR%\bad1_heapdrunk_drunk%%D.txt"
+    )
+)
+
+if false == true (
+    for %%D in (1 5 10 20 50) do (
+        for %%S in (10000 20000 40000 80000 160000) do (
+            "%BINARY%" --test 4 0 %%S %RUNS% 0 --drunk %%D "%OUTPUT_DIR%\bad1_heapdrunk_drunk%%D.txt"
+        )
+    )
+)
+
+if false == true (
+    for %%D in (100 300 500 1000) do (
+        for %%S in (10000 20000 40000 80000 160000) do (
+            "%BINARY%" --test 4 0 %%S %RUN% 0 --drunk %%D "%OUTPUT_DIR%\bad1_heapdrunk_drunk%%D.txt"
+        )
+    )
+)
+
+if false == true (
+    for %%A in (0 1) do (
+        for %%D in (0 1 2 3 4) do (
+            %BINARY% --test %%A 0 80000 %RUNS% %%D %OUTPUT_DIR%\bad2_alg%%A_dist%%D.txt
+        )
+    )
+
+    REM Shell (gap Franka Lazarusa = 1)
+    for %%D in (0 1 2 3 4) do (
+        %BINARY% --test 2 0 80000 %RUNS% %%D --gap 1 %OUTPUT_DIR%\bad2_shell_gap1_dist%%D.txt
+    )
+
+    REM Quick (pivot środkowy = 1)
+    for %%D in (0 1 2 3 4) do (
+        %BINARY% --test 3 0 80000 %RUNS% %%D --pivot 1 %OUTPUT_DIR%\bad2_quick_pivot1_dist%%D.txt
+    )
+
+    REM HeapDrunk (drunk = 500)
+    for %%D in (0 1 2 3 4) do (
+        %BINARY% --test 4 0 80000 %RUNS% %%D --drunk 500 %OUTPUT_DIR%\bad2_heapdrunk_drunk500_dist%%D.txt
+    )
+)
+
+if true == true (
+    for %%t in (0 2 3) do (
+        %BINARY% --test 3 %%t 80000 %RUNS% 0 --pivot 1 %OUTPUT_DIR%\bad3_quick_type%%t.txt
     )
 )
 
